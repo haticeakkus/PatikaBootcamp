faa3e44259f9e576e656069866e0faf884085666
Hatice Akkuş <hticeakks1@gmail.com>
2023-06-20 14:25:21 +0300

    activity recommender according to temperature has been developed

---
+++ Java101/src/activityRecommendation/ActivityRecommendation.java
@@ -0,0 +1,22 @@
+package activityRecommendation;
+
+import java.util.Scanner;
+
+public class ActivityRecommendation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the temperature value: ");
+        int temperature = scanner.nextInt();
+
+        if (temperature < 5) {
+            System.out.println("I suggest going skiing.");
+        } else if (temperature >= 5 && temperature <= 15) {
+            System.out.println("I suggest going to the cinema.");
+        } else if (temperature > 15 && temperature <= 25) {
+            System.out.println("I suggest having a picnic.");
+        } else {
+            System.out.println("I suggest going swimming.");
+        }
+    }
+}
